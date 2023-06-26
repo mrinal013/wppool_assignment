f1695f3d1f4e56ac9d794a3722caa6f737c5928b
Mrinal Haque <mrinalhaque99@gmail.com>
2023-06-27 00:40:53 +0600

    prevent projects block from projects post type

--- wppool-assignment.php
+++ wppool-assignment.php
@@ -62,6 +62,33 @@ add_action( 'plugins_loaded', 'projects_plugin_loaded' );
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function create_block_projects_block_init() {
+
+	// Check if this is the intended custom post type
+	if (is_admin()) {
+		global $pagenow;
+		$typenow = '';
+		if ( 'post-new.php' === $pagenow ) {
+		  if ( isset( $_REQUEST['post_type'] ) && post_type_exists( $_REQUEST['post_type'] ) ) {
+			$typenow = $_REQUEST['post_type'];
+		  };
+		} elseif ( 'post.php' === $pagenow ) {
+		  if ( isset( $_GET['post'] ) && isset( $_POST['post_ID'] ) && (int) $_GET['post'] !== (int) $_POST['post_ID'] ) {
+			// Do nothing
+		  } elseif ( isset( $_GET['post'] ) ) {
+			$post_id = (int) $_GET['post'];
+		  } elseif ( isset( $_POST['post_ID'] ) ) {
+			$post_id = (int) $_POST['post_ID'];
+		  }
+		  if ( $post_id ) {
+			$post = get_post( $post_id );
+			$typenow = $post->post_type;
+		  }
+		}
+		if ($typenow == 'projects') {
+		  return;
+		}
+	  }
+
 	register_block_type( __DIR__ . '/projects/build' );
 }
 add_action( 'init', 'create_block_projects_block_init' );
